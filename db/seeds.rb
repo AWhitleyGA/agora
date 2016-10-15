7fbcb3267e392d2a36ff01234418811c87b55c26
Andrew Whitley <awhitley1233@gmail.com>
2016-10-15 15:14:06 -0400

    set up initial routes/controllers

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,7 +11,8 @@ Product.destroy_all
 
 Category.create([
   {name: "Electronics", img_url: "electronics.png", description: "televisions, phones, speakers, headphones"},
-  {name: "Apparel", img_url: "apparel.png", description: "shirts, pants, skirts, outerwear, shoes, accessories"},
+  {name: "Clothing", img_url: "clothing.png", description: "shirts, pants, skirts, outerwear, accessories"},
+  {name: "Shoes, Belts, and Accessories", img_url: "shoes.png", description: "men's and women's shoes, belts, watches, jewelrey, bags"},
   {name: "Home", img_url: "home.png", description: "furniture, appliances, decorations, lights, outdoor"},
   {name: "Media", img_url: "media.png", description: "movies, games, books, music"},
   {name: "Sports", img_url: "sports.png", description: "training gear, equipment, balls, sports apparel"},
@@ -20,6 +21,8 @@ Category.create([
 Product.create([
   {name: "iPhone 7", price: 599.99, img_url: "iphone_7.png", description: "Apple iPhone 7 with retina display, 5mp camera, touch ID"},
   {name: "Polo Shirt", price: 34.99, img_url: "polo_shirt_1.png", description: "Polo Ralph Lauren button down shirt in light blue, size Large."},
+  {name: "Carrana Lorena Leather Sandal", price: 79.99, img_url: "carrana_lorena_shoe.gif", description: "Make the night with these platform sandals. The juxtaposition of the snake skin golden pattern vamp and the soft black leather ankle strap and counter make this sandal a must have. "},
+  {name: "Men's Navy/Gold Fossil Watch", price: 115.99, img_url: "fossil_watch.png", description: "Men's Fossil watch with navy blue face and strap with gold metal frame and accents"},
   {name: "IKEA Couch", price: 399.99, img_url: "ikea_couch_1.jpg", description: "Multi-piece IKEA couch in black leather with hidden storage"},
   {name: "Star Wars: The Force Awakens", price: 19.99, img_url: "star_wars_episode_7.png", description: "Star Wars Episode VII: The Force Awakens on Blu-Ray HD"},
   {name: "Nike Soccer Ball", price: 15.99, img_url: "soccer_ball.png", description: "Nike Soccer Ball in grey, standard size"},
@@ -29,6 +32,8 @@ CategoryTag.create([
   {product_id: 1, category_id: 1},
   {product_id: 2, category_id: 2},
   {product_id: 3, category_id: 3},
-  {product_id: 4, category_id: 4},
-  {product_id: 5, category_id: 5},
+  {product_id: 4, category_id: 3},
+  {product_id: 5, category_id: 4},
+  {product_id: 6, category_id: 5},
+  {product_id: 7, category_id: 6},
 ])
