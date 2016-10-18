297b3b01d721d79d4fb80d3bc673601965f9f9e1
Andrew Whitley <awhitley1233@gmail.com>
2016-10-18 15:42:27 -0400

    added user homepage

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,6 +1,10 @@
 class ProductsController < ApplicationController
   def show
-    @category = Category.find(params[:category_id])
-    @product = Product.find(params[:id])
+    if params[:category_id]
+      @category = Category.find(params[:category_id])
+      @product = Product.find(params[:id])
+    else
+      @product = Product.find(params[:id])
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,14 @@ Rails.application.routes.draw do
 
 devise_for :users
 
+resources :users do
+  resources :products
+end
+
 resources :categories do
   resources :products
 end
+
 
 get "/account" => "users#account"
 get "/cart" => "users#cart"
