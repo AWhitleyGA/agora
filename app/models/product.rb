dbad0c82c93546ad20156bcd993d2b8642a7be42
Andrew Whitley <awhitley1233@gmail.com>
2016-10-18 12:04:40 -0400

    made/linked User model, updated seeds to add users/user_id to products

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,6 @@
 class Product < ApplicationRecord
   has_many :category_tags
   has_many :categories, through: :category_tags
+
+  belongs_to :user
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
 
+devise_for :users
+
 resources :categories do
   resources :products
 end
 
+get "/account" => "users#account"
+get "/cart" => "users#cart"
 root "home#index"
 
 end
