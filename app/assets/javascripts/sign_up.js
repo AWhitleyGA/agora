9a91215ba30da7a437c76c47193999077407b7fb
Andrew Whitley <awhitley1233@gmail.com>
2016-10-19 10:00:10 -0400

    user sign-up working

---
+++ app/assets/javascripts/sign_up.js
@@ -0,0 +1,21 @@
+$(document).ready(function() {
+
+
+
+  $('#next_1').on('click', function(){
+    if ( $('.sign-up-form-1').find('.form_field').get().every(field => field.value != "") ){
+      $('.sign-up-form-1').hide()
+      $('.sign-up-form-2').show()
+    }
+  })
+
+
+
+
+  $('#next_2').on('click', function(){
+    $('.sign-up-form-2').hide()
+    $('.sign-up-form-3').show()
+  })
+
+
+})
--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -3,6 +3,6 @@ class RegistrationsController < Devise::RegistrationsController
   private
 
   def sign_up_params
-    params.require(:user).permit(:username, :email, :password, :password_confirmation)
+    params.require(:user).permit(:username, :email, :password, :password_confirmation, :first_name, :last_name, :phone_number, :billing_address_1, :billing_address_2, :mailing_address_1, :mailing_address_2)
   end
 end
