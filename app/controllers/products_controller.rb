4728cc529b434090b5ec99c804346c9effecf746
Andrew Whitley <awhitley1233@gmail.com>
2016-10-19 12:57:45 -0400

    user can make postings with categories

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,4 +1,5 @@
 class ProductsController < ApplicationController
+
   def show
     if params[:category_id]
       @category = Category.find(params[:category_id])
@@ -7,4 +8,32 @@ class ProductsController < ApplicationController
       @product = Product.find(params[:id])
     end
   end
+
+  def new
+    @product = Product.new
+    @categories = Category.all
+  end
+
+  def create
+    @product = Product.new(product_params)
+    @product.user_id = current_user.id
+    @product.save
+    @categories = Category.all
+    @categories.each do |category|
+      if params.keys.include?(category.id.to_s)
+        CategoryTag.create(
+          product_id: @product.id,
+          category_id: category.id
+        )
+      end
+    end
+    redirect_to user_product_path(current_user, @product)
+  end
+
+  private
+
+  def product_params
+    params.require(:product).permit(:name, :price, :description, :img_url)
+  end
+
 end
