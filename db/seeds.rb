8202c8ddc9c045c6eac330815345f26c19f872cf
Andrew Whitley <awhitley1233@gmail.com>
2016-11-16 16:27:21 -0500

    style update

--- db/seeds.rb
+++ db/seeds.rb
@@ -19,13 +19,13 @@ User.create([
 ])
 
 Category.create([
-  {name: "Electronics", img_url: "electronics.png", description: "televisions, phones, speakers, headphones"},
-  {name: "Clothing", img_url: "clothing.png", description: "shirts, pants, skirts, outerwear, accessories"},
-  {name: "Shoes, Belts, and Accessories", img_url: "shoes.png", description: "men's and women's shoes, belts, watches, jewelrey, bags"},
-  {name: "Home and Outdoors", img_url: "home.png", description: "furniture, appliances, decorations, lights, outdoor"},
-  {name: "Media", img_url: "media.png", description: "movies, games, books, music"},
-  {name: "Sports", img_url: "sports.png", description: "training gear, equipment, balls, sports apparel"},
-  {name: "Miscellaneous", img_url: "miscellaneous.png", description: "Miscellaneous products, services, and offers"}
+  {name: "Electronics", img_url: "electronics.jpg", description: "televisions, phones, speakers, headphones"},
+  {name: "Clothing", img_url: "clothing.jpg", description: "shirts, pants, skirts, outerwear"},
+  {name: "Shoes, Belts, and Accessories", img_url: "accessories.jpg", description: "shoes, belts, watches, jewelrey, bags"},
+  {name: "Home and Outdoors", img_url: "home.jpg", description: "furniture, appliances, decorations, lights"},
+  {name: "Media", img_url: "media.jpg", description: "movies, games, books, music"},
+  {name: "Sports", img_url: "sports.jpg", description: "training gear, equipment, balls, sports apparel"},
+  {name: "Miscellaneous", img_url: "miscellaneous.jpg", description: "Miscellaneous products, services, and offers"}
 ])
 
 Product.create([
